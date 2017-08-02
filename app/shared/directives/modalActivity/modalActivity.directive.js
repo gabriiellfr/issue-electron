d10f7e8beed2d45faeb74b05c8cc3a07bfdcf5db
olivermathe <mathe_13@hotmail.com>
2017-08-02 20:57:39 -0300

    Passando modals para directivas angular

---
+++ app/shared/directives/modalActivity/modalActivity.directive.js
@@ -0,0 +1,8 @@
+'use strict';
+
+app.directive('modalActivity', function () {
+    return {
+        restrict: 'E',
+        templateUrl: 'app/shared/directives/modalActivity/modalActivity.html'
+    };
+});
---
+++ app/shared/directives/modalEdit/modalEdit.directive.js
@@ -0,0 +1,8 @@
+'use strict';
+
+app.directive('modalEdit', function () {
+    return {
+        restrict: 'E',
+        templateUrl: 'app/shared/directives/modalEdit/modalEdit.html'
+    };
+});
